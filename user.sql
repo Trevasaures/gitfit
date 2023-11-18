99528cc42539a837319bdb57d246e2685cb2a210
Lifeisforty2 <perecame8700@gmail.com>
2023-11-18 15:02:20 -0600

    db update for weight tracking graph

--- user.sql
+++ user.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 17, 2023 at 10:43 PM
+-- Generation Time: Nov 18, 2023 at 10:00 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -47,7 +47,29 @@ CREATE TABLE `users_table` (
 
 INSERT INTO `users_table` (`name`, `age`, `gender`, `email`, `password`, `current_weight`, `target_weight`, `height_inches`, `id`, `username`, `created_at`) VALUES
 ('cameron perez', 27, 'male', 'perecame8700@gmail.com', '$2y$10$lv4EbAlYzIp8huPkO/wU0uVrfLNTXFL8gpFi2O2YdeGN51As4jNoK', 180, 190, 72, 1, 'camperez', '2023-11-17 18:15:38'),
-('cameron perez', 27, 'male', 'cameron.perez@my.utsa.edu', '$2y$10$siCDRFWWJ91q.sNnLupO0eFU1A5tOO4Xx730E8zT1DENJsD5Lyf4C', 180, 190, 72, 2, 'life', '2023-11-17 19:32:26');
+('cameron perez', 27, 'male', 'cameron.perez@my.utsa.edu', '$2y$10$siCDRFWWJ91q.sNnLupO0eFU1A5tOO4Xx730E8zT1DENJsD5Lyf4C', 187, 190, 72, 2, 'life', '2023-11-17 19:32:26'),
+('timmy tim', 27, 'male', 'tim@gmail.com', '$2y$10$MODK1R/gofi9sbUU9MD6.eZvzAlA8U701rezFZiNmGXQPJoGqiz3y', 180, 190, 72, 3, 'tim', '2023-11-18 02:09:18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `weight_records`
+--
+
+CREATE TABLE `weight_records` (
+  `weight_id` int(11) NOT NULL,
+  `id` int(11) DEFAULT NULL,
+  `weight_date` date DEFAULT NULL,
+  `weight` decimal(5,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `weight_records`
+--
+
+INSERT INTO `weight_records` (`weight_id`, `id`, `weight_date`, `weight`) VALUES
+(1, 2, '2023-11-17', 189.00),
+(3, 3, '2023-11-17', 191.00);
 
 --
 -- Indexes for dumped tables
@@ -62,6 +84,13 @@ ALTER TABLE `users_table`
   ADD UNIQUE KEY `UNIQUE_USERNAM` (`username`);
 
 --
+-- Indexes for table `weight_records`
+--
+ALTER TABLE `weight_records`
+  ADD PRIMARY KEY (`weight_id`),
+  ADD UNIQUE KEY `id` (`id`,`weight_date`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -69,7 +98,23 @@ ALTER TABLE `users_table`
 -- AUTO_INCREMENT for table `users_table`
 --
 ALTER TABLE `users_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `weight_records`
+--
+ALTER TABLE `weight_records`
+  MODIFY `weight_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `weight_records`
+--
+ALTER TABLE `weight_records`
+  ADD CONSTRAINT `weight_records_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users_table` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
