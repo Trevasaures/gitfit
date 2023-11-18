0f5ed5ffaa08f5b2d2d733c0e2b78685019ae6ce
Lifeisforty2 <perecame8700@gmail.com>
2023-11-17 20:19:17 -0600

    added weight tracking and graph

--- index.php
+++ index.php
@@ -16,6 +16,15 @@ $stmt = $pdo->prepare("SELECT * FROM users_table WHERE id = ?");
 $stmt->bindParam(1, $id); // bindParam() binds a variable to a parameter
 $stmt->execute();
 $userData = $stmt->fetch(PDO::FETCH_ASSOC); // fetch() returns a single row 
+
+// get weight data from database
+$weightStmt = $pdo->prepare("SELECT weight FROM weight_records WHERE id = ? ORDER BY weight_date ASC");
+$weightStmt->bindParam(1, $id);
+$weightStmt->execute();
+$latestWeight = $weightStmt->fetch(PDO::FETCH_ASSOC);
+
+// check if theres a wight record for today
+$currentWeight = $latestWeight ? $latestWeight['weight'] : 'No record for today!';
 ?>
 
 <!DOCTYPE HTML>
@@ -32,7 +41,7 @@ $userData = $stmt->fetch(PDO::FETCH_ASSOC); // fetch() returns a single row
     <h1>Hello, GitFit App!</h1>
     <!-- Display user stats, progress, and other general information here -->
     <p>User: <?php echo htmlspecialchars($userData['username']); ?></p> <!-- htmlspecialchars() converts special characters to HTML entities -->
-    <p>Weight: <?php echo htmlspecialchars($userData['current_weight']); ?></p>
+    <p>Weight: <?php echo htmlspecialchars($currentWeight); ?></p>
     <p>Target Weight: <?php echo htmlspecialchars($userData['target_weight']); ?></p>
     <p>Height: <?php echo htmlspecialchars($userData['height_inches']); ?></p>
 
@@ -41,7 +50,7 @@ $userData = $stmt->fetch(PDO::FETCH_ASSOC); // fetch() returns a single row
     <h2>Select what you would like to do!</h2>
 
     <!-- Navigation links to other sections -->
-    <a href="weight.html">Weight Tracking</a>
+    <a href="weight.php">Weight Tracking</a>
     <a href="food.html">Food Calculator</a>
     <a href="notes.html">Note Records</a>
     <a href="workouts.html">Workout Tracking</a>
---
+++ weight.php
@@ -0,0 +1,106 @@
+<?php
+session_start();
+include 'dbconfig.php';
+
+$userId = $_SESSION['id'];
+$weightUpdated = false;
+$errorMessage = '';
+
+// handle weight update
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['current_weight'])) {
+    $newWeight = $_POST['current_weight'];
+
+    // insert new weight record into database
+    // if a record for today already exists, update it
+    // on duplicate key update means that if the primary key already exists, update the record
+    $stmt = $pdo->prepare("INSERT INTO weight_records (id, weight_date, weight) VALUES (?, CURDATE(), ?) ON DUPLICATE KEY UPDATE weight = ?");
+    if ($stmt->execute([$userId, $newWeight, $newWeight])) {
+        $weightUpdated = true;
+    } else {
+        $errorMessage = "Error updating weight!";
+    }
+}
+
+
+// userId contains the user's id
+$weights = [];
+$dates = [];
+
+$stmt = $pdo->prepare("SELECT * FROM weight_records WHERE id = ? ORDER BY weight_date ASC");
+$stmt->execute([$userId]);
+
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $weights[] = $row['weight'];
+    $dates[] = $row['weight_date'];
+}
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ctrl+Alt+Elite</title>
+    <!-- Link to CSS file -->
+    <link rel="stylesheet" href="css/styles.css">
+    <!-- add chart.js -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> 
+
+</head>
+
+<body>
+    <!-- Main header for applicaiton -->
+    <h1 id="h1">Welcome to the weight tracker!</h1>
+
+    <!-- Display user stats, progress, and other general information here -->
+    <!-- Weight Update Form -->
+    <form action="weight.php" method="post">
+        <label for="current_weight">Current Weight:</label>
+        <input type="number" name="current_weight" id="current_weight" required>
+        <input type="submit" value="Update Weight">
+    </form>
+
+    <?php if ($weightUpdated): ?>
+        <p>Weight updated successfully!</p>
+    <?php elseif (!empty($errorMessage)): ?>
+        <p style="color:red;"><?php echo $errorMessage; ?></p>
+    <?php endif; ?>
+    
+    <!-- Weight Chart -->
+    <script>
+        var weightDates = <?php echo json_encode($dates); ?>;
+        var weightValues = <?php echo json_encode($weights); ?>;
+    </script>
+    <canvas id="weightChart"></canvas>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+        var ctx = document.getElementById('weightChart').getContext('2d');
+        var weightChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: weightDates,
+                datasets: [{
+                    label: 'Weight',
+                    data: weightValues,
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)'
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            suggestedMax: 200
+                        }
+                    }]
+                }
+            }
+        });
+    </script>
+
+    <!-- Back button -->
+    <a href="index.php" class="back-button">&#8678; Back to Main</a>
+</body>
+</html>
